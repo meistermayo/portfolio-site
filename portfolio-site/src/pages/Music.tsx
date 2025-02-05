dad54d42df35958ff4019decc038c0813667838f
meistermayo <meistermayo@gmail.com>
2025-02-05 13:00:15 -0600

    Forgot the headers
    got creative with organization

--- portfolio-site/src/pages/Music.tsx
+++ portfolio-site/src/pages/Music.tsx
@@ -10,15 +10,18 @@ export default function Music(){
             Recently I've been making various flavors of EDM heavily focusing on sound design, making novel bass sounds. 
             I've made lots of music of various genres. I've made orchestral tracks, lofi, chiptunes, and various flavors of EDM.
             </div>
-            <div className="container">
-                <SoundcloudCard track_id="1743224562"/>
-                <SoundcloudCard track_id="1683545013"/>
-                <SoundcloudPlaylistCard playlist_id="1658762536"/>
-                <SoundcloudPlaylistCard playlist_id="1107505642"/>
-                <SoundcloudPlaylistCard playlist_id="911011003"/>
-                <SoundcloudPlaylistCard playlist_id="1682587152"/>
-                <SoundcloudPlaylistCard playlist_id="1678633356"/>
-            </div>
+                <h1>Bass Music</h1>
+                <div className="container">
+                    <SoundcloudCard track_id="1743224562"/>
+                    <SoundcloudCard track_id="1683545013"/>
+                </div>
+                    <SoundcloudPlaylistCard playlist_id="1658762536"/>
+                    <h1>Orchestral/Cinematic Music</h1>
+            <SoundcloudPlaylistCard playlist_id="1682587152"/>
+            <h1>Lofi</h1>
+            <SoundcloudPlaylistCard playlist_id="911011003"/>
+            <h1>Chiptune</h1>
+            <SoundcloudPlaylistCard playlist_id="1107505642"/>
         </div>
     )
 }
