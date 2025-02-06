3420ae74660f8ab6416a76ca3d8fd75d322cc760
meistermayo <meistermayo@gmail.com>
2025-02-05 23:33:20 -0600

    mobile scrollbar

--- portfolio-site/src/components/Topbar.tsx
+++ portfolio-site/src/components/Topbar.tsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom"
 import "./style/Topbar.css"
 
 export default function Topbar() {
+    const isMobile = window.innerWidth <= 768;
+
     return (
         <div className="topbar">
             <h3>Luke Mayo<br/></h3>
             
-            <div className="links">
+            <div className={isMobile ? "linksMobile" : "links"}>
                 <Link to="">About</Link>
                 <Link to="Work">Work</Link>
                 <Link to="Programming">Programming</Link>
