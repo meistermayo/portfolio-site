2815870e4222355454262a442f71d0acb839381f
meistermayo <meistermayo@gmail.com>
2025-02-07 10:05:40 -0600

    unused

--- portfolio-site/src/components/ArtImage.tsx
+++ portfolio-site/src/components/ArtImage.tsx
@@ -7,7 +7,6 @@ interface Props {
 export default function ArtImage({src}: Props) {
     return (
         <div className="imageWrapper">
-            <div className="colorOverlay"></div>
             <img className="image" src={src}/>
         </div>
     )
