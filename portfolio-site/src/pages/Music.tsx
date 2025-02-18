a01d3229cf8ead1e186718c3b2f382fceb9cc2e3
meistermayo <meistermayo@gmail.com>
2025-02-18 08:50:55 -0600

    condensed music section

--- portfolio-site/src/pages/Music.tsx
+++ portfolio-site/src/pages/Music.tsx
@@ -7,7 +7,7 @@ import "./style/Music.css"
 export default function Music(){
     return (
         <div>
-            <PageHeader title="Bass Music">
+            <PageHeader title="Music">
                 I love making music from all sides. I do composition, sound design, and mixing and mastering on the music I make. 
                 Recently I've been making various flavors of EDM heavily focusing on sound design, making novel bass sounds. 
                 I've made lots of music of various genres. I've made orchestral tracks, lofi, chiptunes, and various flavors of EDM.
@@ -16,17 +16,25 @@ export default function Music(){
             <div style={{paddingTop: 32, paddingBottom:32}}>
 
                 <div className="musicContainer">
-                    <SoundcloudCard track_id="1743224562"/>
-                    <SoundcloudCard track_id="1683545013"/>
+                    <div>
+                        <h3>EDM / Bass Music</h3>
+                        <SoundcloudPlaylistCard playlist_id="1658762536"/>
+                    </div>
+                    <div>
+                        <h3>Orchestral</h3>
+                        <SoundcloudPlaylistCard playlist_id="1682587152"/>
+                    </div>
+                    <div>
+                        <h3>Lofi</h3>
+                        <SoundcloudPlaylistCard playlist_id="911011003"/>
+                    </div>
+
+                    <div>
+                        <h3>Chiptune</h3>
+                        <SoundcloudPlaylistCard playlist_id="1107505642"/>
+                    </div>
                 </div>
 
-                <SoundcloudPlaylistCard playlist_id="1658762536"/>
-                <h1>Orchestral/Cinematic Music</h1>
-                <SoundcloudPlaylistCard playlist_id="1682587152"/>
-                <h1>Lofi</h1>
-                <SoundcloudPlaylistCard playlist_id="911011003"/>
-                <h1>Chiptune</h1>
-                <SoundcloudPlaylistCard playlist_id="1107505642"/>
             </div>
         </div>
     )
